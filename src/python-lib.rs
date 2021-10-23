ef28f0c3fbe4f74dac0905e31ec108c48de3a465
Henry Nguyen <henry2833@gmail.com>
2021-10-22 23:14:21 -0700

    updated python lib

--- src/python-lib.rs
+++ src/python-lib.rs
@@ -9,7 +9,7 @@ mod main_db;
 
 pub use main_db::{DbManager, Range};
 use cpython;
-use cpython::{PyBytes, PyResult, Python};
+use cpython::{PyBytes, PyResult, Python, PyObject, PyNone, ToPyObject, ObjectProtocol, PyDict, PythonObject, PyList};
 use std::cmp::Ordering;
 use crate::suitable_data_type::SuitableDataType;
 use crate::bytes_serializer::{BytesSerialize, FromReader};
@@ -26,8 +26,8 @@ use std::convert::TryInto;
 struct BusStruct {
     timestamp: u64,
     trip_id: u32,
-    start_date: [u8;8],
-    route_id: [u8;5],
+    start_date: [u8; 8],
+    route_id: [u8; 5],
     latitude: f64,
     longitude: f64,
     current_stop_sequence: u8,
@@ -36,16 +36,33 @@ struct BusStruct {
     direction_id: bool,
 }
 
+impl BusStruct {
+    fn kv_iter<F: Fn(&str, PyObject)>(&self,_p: Python, callable: F) {
+        callable("timestamp", ToPyObject::to_py_object(&self.timestamp, _p).into_object());
+        callable("trip_id", ToPyObject::to_py_object(&self.trip_id, _p).into_object());
+        callable("start_date", PyBytes::new(_p, &self.start_date).into_object());
+        callable("route_id", PyBytes::new(_p, &self.route_id).into_object());
+        callable("latitude", ToPyObject::to_py_object(&self.latitude, _p).into_object());
+        callable("longitude", ToPyObject::to_py_object(&self.longitude, _p).into_object());
+        callable("current_stop_sequence", ToPyObject::to_py_object(&self.current_stop_sequence, _p).into_object());
+        callable("stop_id", ToPyObject::to_py_object(&self.stop_id, _p).into_object());
+        callable("vehicle_id", ToPyObject::to_py_object(&self.vehicle_id, _p).into_object());
+        callable("direction_id", ToPyObject::to_py_object(&self.direction_id, _p).into_object());
+    }
+}
+
 impl SuitableDataType for BusStruct {
     fn first(&self) -> u64 {
         self.timestamp
     }
 }
+
 impl BytesSerialize for BusStruct {}
 gen_suitable_data_type_impls!(BusStruct);
 unsafe fn raw_ptr_to_slice<'a, T, A: 'a>(ptr: *mut T, _lifetime: &A) -> &'a mut [u8] {
     std::slice::from_raw_parts_mut(ptr as *mut u8, std::mem::size_of::<T>())
 }
+
 impl FromReader for BusStruct {
     fn from_reader<R: Read>(r: &mut R) -> Self {
         let mut buf = MaybeUninit::<BusStruct>::uninit();
@@ -68,6 +85,16 @@ unsafe fn init_dbptr() -> &'static mut DbManager<BusStruct> {
 }
 
 
+fn str_to_slice<const T: usize>(a: &str) -> [u8; T] {
+    if a.len() > T {
+        panic!("Passed length exceeds allocated buffer");
+    }
+
+    let mut buf = [0u8; T];
+    let buf_same_len = &mut buf[0..a.len()];
+    buf_same_len.copy_from_slice(a.as_bytes());
+    buf
+}
 fn store(_p: Python, timestamp: u64,
          trip_id: u32,
          start_date: &str,
@@ -77,9 +104,9 @@ fn store(_p: Python, timestamp: u64,
          current_stop_sequence: u8,
          stop_id: u16,
          vehicle_id: u16,
-         direction_id: bool,) -> PyResult<cpython::NoArgs>{
-    let start_date: [u8; 8] = start_date.as_bytes().try_into().unwrap();
-    let route_id: [u8; 5] = route_id.as_bytes().try_into().unwrap();
+         direction_id: bool, ) -> PyResult<cpython::NoArgs> {
+    let start_date: [u8; 8] = str_to_slice(start_date);
+    let route_id: [u8; 5] = str_to_slice(route_id);
     let bus = BusStruct {
         timestamp,
         trip_id,
@@ -92,12 +119,28 @@ fn store(_p: Python, timestamp: u64,
         stop_id,
         vehicle_id,
     };
-    unsafe {init_dbptr()}.store(bus);
+    unsafe { init_dbptr() }.store(bus);
     Ok(cpython::NoArgs)
 }
 
+
+fn get(_p: Python, pkey: u64) -> PyResult<PyList> {
+    let dbm = unsafe { init_dbptr() };
+    let result = dbm.get_in_all(pkey..=pkey);
+
+    let py_result: Vec<_> = result.into_iter().map(|a| {
+        let dict = PyDict::new(_p);
+        a.kv_iter(_p, |name, value| { dict.set_item(_p, name, value); });
+        dict.into_object()
+    }).collect();
+
+    Ok(PyList::new(_p, py_result.as_slice()))
+}
+
+
+
 fn debug_dump(_p: Python) -> PyResult<cpython::NoArgs> {
-    let db = unsafe {init_dbptr()};
+    let db = unsafe { init_dbptr() };
     println!("{:?}", db);
     Ok(cpython::NoArgs)
 }
@@ -113,6 +156,8 @@ py_module_initializer!(libpythonlib, |py, m| {
     stop_id: u16,
     vehicle_id: u16,
     direction_id: bool)))?;
+
     m.add(py, "debug_dump", py_fn!(py, debug_dump()))?;
+    m.add(py, "get", py_fn!(py, get(pkey: u64)))?;
     Ok(())
 });
