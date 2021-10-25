98416b05f0dbbb2f68fe60892398deade2e52965
Henry Nguyen <henry2833@gmail.com>
2021-10-24 18:30:28 -0700

    added buffer pool

---
+++ src/buffer_pool.rs
@@ -0,0 +1,91 @@
+use std::collections::HashMap;
+
+use crate::dbbase::DbBase;
+use crate::suitable_data_type::SuitableDataType;
+use std::iter::FromIterator;
+
+pub struct BufferPool<T: SuitableDataType> {
+    // Maps from location -> number of uses since last eviction
+    lru: HashMap<u64, u32>,
+
+    // Maps from location -> actual in-memory database
+    buffer_pool: HashMap<u64, DbBase<T>>,
+}
+
+impl<T: SuitableDataType> Default for BufferPool<T> {
+    fn default() -> Self {
+        Self { lru: Default::default(), buffer_pool: Default::default() }
+    }
+}
+
+impl<T: SuitableDataType> BufferPool<T> {
+    const MAX_BUFFERPOOL_SIZE: usize = 10;
+
+    pub fn load_page<Loader: FnOnce() -> DbBase<T>>(&mut self, location: u64, loader: Loader) -> &mut DbBase<T> {
+        self.evict_if_necessary();
+        self.lru.entry(location).and_modify(|e| { *e += 1 }).or_insert(1);
+        self.buffer_pool.entry(location).or_insert_with(loader)
+    }
+
+    pub fn evict_if_necessary(&mut self) {
+        if self.buffer_pool.len() > Self::MAX_BUFFERPOOL_SIZE {
+            // Find least recently used items
+            let mut lru = Vec::from_iter(self.lru.iter().map(|(a, b)| (*a, *b)));
+            lru.sort_by_key(|(loc, uses)| *uses);
+            for (loc_to_remove, _) in lru {
+                if self.buffer_pool.len() > Self::MAX_BUFFERPOOL_SIZE {
+                    self.lru.remove(&loc_to_remove);
+                    self.buffer_pool.remove(&loc_to_remove);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+    use super::*;
+
+    fn default_loader() -> DbBase<DataType> {
+        DbBase::<DataType>::default()
+    }
+
+    #[test]
+    fn test_buffer_pool1() {
+        type MyBufferPool = BufferPool<DataType>;
+        // Required for this test to work
+        assert!(MyBufferPool::MAX_BUFFERPOOL_SIZE >= 5);
+        assert!(MyBufferPool::MAX_BUFFERPOOL_SIZE < 1000000);
+
+        let mut buffer_pool = MyBufferPool::default();
+
+        // Load max size + 1 elements into pool
+        for i in 0..MyBufferPool::MAX_BUFFERPOOL_SIZE {
+            buffer_pool.load_page(i as u64, default_loader);
+        }
+
+        // Loader should not be called, as there should be no evictions
+        buffer_pool.load_page(0 as u64, || panic!());
+        buffer_pool.load_page(1 as u64, || panic!());
+
+        let mut called = false;
+        buffer_pool.load_page(1000000, || {
+            called = true;
+            default_loader()
+        });
+        assert!(called);
+
+        // 1 should still be in pool, because it was most recently used
+        buffer_pool.load_page(1 as u64, || panic!());
+        buffer_pool.load_page(0 as u64, || panic!());
+
+        for i in 0..10 {
+            buffer_pool.load_page(i + 10000000, default_loader);
+        }
+        assert!(buffer_pool.buffer_pool.len() <= MyBufferPool::MAX_BUFFERPOOL_SIZE + 1);
+    }
+}
---
+++ src/db_manager.rs
@@ -0,0 +1,138 @@
+// todo: compression, secondary indexes
+
+
+
+use std::cmp::Ordering;
+use std::collections::{BTreeSet, HashMap, HashSet, VecDeque};
+use std::fmt::{Debug, Display, Formatter};
+use std::io::{Cursor, Read, Seek, SeekFrom, Write};
+use std::iter::FromIterator;
+use std::ops::RangeBounds;
+
+use crate::bytes_serializer::{BytesSerialize, FromReader};
+use crate::chunk_header::ChunkHeader;
+use crate::dbbase::DbBase;
+use crate::range::Range;
+use crate::suitable_data_type::SuitableDataType;
+use crate::buffer_pool::BufferPool;
+
+#[allow(unused)]
+fn setup_logging() {
+    env_logger::init();
+}
+
+
+// Provides higher level database API's -- automated flushing to disk, query functions for previously flushed chunks
+pub struct DbManager<T: SuitableDataType, Writer: Write + Seek + Read = Cursor<Vec<u8>>> {
+    db: DbBase<T>,
+    previous_headers: Vec<(u64, ChunkHeader<T>)>,
+    buffer_pool: BufferPool<T>,
+    output_stream: Writer,
+}
+
+impl<T: SuitableDataType, Writer: Write + Seek + Read> DbManager<T, Writer> {
+    // Maximum tuples we can hold in memory. After this amount, we empty to disk.
+    const FLUSH_CUTOFF: usize = 5;
+
+    // Constructs a DbManager instance from a DbBase and an output writer (like a file)
+    pub fn new(writer: Writer) -> Self {
+        Self { output_stream: writer, previous_headers: Vec::default(), db: Default::default(), buffer_pool: Default::default() }
+    }
+
+    fn check_should_flush(&mut self) {
+        if self.db.len() >= Self::FLUSH_CUTOFF {
+            let stream_pos = self.output_stream.stream_position().unwrap();
+            let (header, _) = self.db.force_flush(&mut self.output_stream);
+            self.previous_headers.push((stream_pos, header));
+        }
+    }
+
+    // Store tuple into the database, flushing to disk if the in-memory database exceeds FLUSH_CUTOFF
+    pub fn store(&mut self, t: T) {
+        self.db.store(t);
+        self.check_should_flush();
+    }
+
+    pub fn store_and_replace(&mut self, t: T) -> Option<T> {
+        let val = self.db.store_and_replace(t);
+        self.check_should_flush();
+        val
+    }
+
+    // Filter all chunk headers that can possibly satisfy range, and return their locations in the stream
+    fn chunks_in_range<RB: RangeBounds<u64>>(headers: &Vec<(u64, ChunkHeader<T>)>, range: &RB) -> Vec<u64> {
+        headers.iter().filter_map(|(pos, h)|
+            h.limits.overlaps(range).then(|| *pos)).collect()
+    }
+
+    fn load_page(&mut self, page_loc: u64) -> &mut DbBase<T> {
+        let loader = || {
+            self.output_stream.seek(SeekFrom::Start(page_loc)).unwrap();
+            DbBase::<T>::from_reader(&mut self.output_stream)
+        };
+
+        self.buffer_pool.load_page(page_loc, loader)
+    }
+    // Iterate through all the previously flushed chunk headers and look for all tuples contained in range `RB`
+    pub fn get_in_all<RB: RangeBounds<u64>>(&mut self, range: RB) -> Vec<T> {
+        let ok_chunks = Self::chunks_in_range(&self.previous_headers, &range);
+        let mut cln = BTreeSet::new();
+        for pos in ok_chunks {
+            let db = self.load_page(pos);
+            let range = db.key_range(&range);
+
+            for j in range {
+                cln.replace(j.clone());
+            }
+        };
+        self.db.sort_self();
+
+        for j in self.db.key_range(&range) {
+            cln.replace(j.clone());
+        }
+
+        cln.into_iter().collect()
+    }
+
+    #[cfg(test)]
+    pub fn get_output_stream_len(&mut self) -> usize {
+        self.output_stream.stream_position().unwrap() as usize
+    }
+    #[cfg(test)]
+    pub fn force_flush(&mut self) {
+        let stream_pos = self.output_stream.stream_position().unwrap();
+        let (header, _) = self.db.force_flush(&mut self.output_stream);
+        self.previous_headers.push((stream_pos, header));
+    }
+}
+
+impl<T: SuitableDataType, Writer: Write + Seek + Read> Debug for DbManager<T, Writer> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("DbManager")
+            .field("current_data", &self.db)
+            .field("prev_headers", &self.previous_headers)
+            .finish()
+    }
+}
+
+impl<T: SuitableDataType> Default for DbManager<T> {
+    fn default() -> Self {
+        let db = DbBase::default();
+        Self { db, output_stream: Cursor::new(Vec::new()), buffer_pool: Default::default(), previous_headers: Default::default() }
+    }
+}
+
+
+// Checks that slice has no duplicate values
+pub fn assert_no_dups<T: PartialOrd + Debug>(a: &[T]) -> bool {
+    debug_assert!(a.is_sorted());
+    for window in a.windows(2) {
+        match window {
+            [i, j] if i.partial_cmp(j) == Some(Ordering::Less) => {}
+            _ => {
+                panic!("Duplicated primary key value {:?}", window);
+            }
+        }
+    }
+    true
+}
